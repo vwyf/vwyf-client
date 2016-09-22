9f8d79d42a812b569c679848ba94ebe052b865a9
parano <paranoyang@gmail.com>
2016-09-21 23:10:56 -0700

    caching wip

--- answers_cache.py
+++ answers_cache.py
@@ -2,6 +2,10 @@
 import sqlite3
 # part of pymongo library, used for generating mongo objectId on client side
 import bson
+import datetime
+
+def ctime():
+  datetime.datetime.now().ctime()
 
 def init():
   conn = sqlite3.connect('answers.db')
@@ -9,16 +13,28 @@ def init():
   conn.close()
 
 def createTable(conn):
+  print "Initializing Table"
   c = conn.cursor()
-  c.execute('''create table if not exists answers (
-      answerId TEXT,
+  c.execute('''CREATE TABLE IF NOT EXISTS answers (
+      id INTEGER AUTOINCREMENT,
       questionsId TEXT,
       answer INTEGER,
       createdAt TIMESTAMP,
       savedToServer INTEGER DEFAULT 0
       )''')
   conn.commit()
-  print "done"
+
+def addNewAnswer(questionId, answer):
+  now = ctime()
+  c = conn.cursor()
+  c.execute('''INSERT INTO answers (questionId, answer, createdAt) VALUES (?, ?, ?)''',
+      (questionId, answer, now))
+  conn.commit()
+  print "{now!s}: added new answer: {questionId!s} {answer!s}".format(**locals())
+
+def syncAnswers(batch = 10):
+  #todo
+  return
 
 if __name__ == "__main__":
   init()
