121a17b688ee2c130014390a72e5ff701ec37b6a
parano <paranoyang@gmail.com>
2016-10-04 19:17:29 -0700

    update url

--- apiclient.py
+++ apiclient.py
@@ -13,7 +13,7 @@ logging.basicConfig(filename='vwyf.log',level=logging.INFO)
 #   {'questionId': 'v3umMe2QdAa6HngJ6', 'answer': 'B', 'createdAt': 1474516761283 }
 # ];
 
-base_url = 'http://138.68.46.208'
+base_url = 'http://vwyf.1x1.cm'
 # base_url = 'http://localhost:3000'
 questions_url = base_url + '/questions'
 answers_url = base_url + '/answers'
